e9433db85f8ba09b3732e1d22463e470ff61284f
revbucket <mattjordan.mail@gmail.com>
2016-11-08 16:29:54 -0500

    Updated required zendesk package

--- package.js
+++ package.js
@@ -14,7 +14,7 @@ Package.onUse(function(api) {
   // Export Accounts (etc) to packages using this one.
   api.imply('accounts-base@1.2.13', ['client', 'server']);
   api.use('accounts-oauth@1.1.13', ['client', 'server']);
-  api.use('revbucket:zendesk@1.1.0', ['client', 'server']);
+  api.use('revbucket:zendesk@1.1.3', ['client', 'server']);
 
   api.use('http@1.2.9_1', ['client', 'server']);
 
