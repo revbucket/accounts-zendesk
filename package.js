cc83c5dfbba1243a5c18e8a9cc896dc15861a61d
revbucket <mattjordan.mail@gmail.com>
2016-11-21 18:43:33 -0500

    incremented versions

--- package.js
+++ package.js
@@ -1,8 +1,8 @@
 Package.describe({
   summary: "Login service for Zendesk accounts",
-  version: "1.1.1",
+  version: "1.1.2",
   name: "revbucket:accounts-zendesk",
-  version: "1.1.1",
+  version: "1.1.2",
   documentation: "README.md",
   git: "https://github.com/revbucket/accounts-zendesk.git"
 
@@ -14,7 +14,7 @@ Package.onUse(function(api) {
   // Export Accounts (etc) to packages using this one.
   api.imply('accounts-base@1.2.13', ['client', 'server']);
   api.use('accounts-oauth@1.1.13', ['client', 'server']);
-  api.use('revbucket:zendesk@1.1.3', ['client', 'server']);
+  api.use('revbucket:zendesk@1.1.4', ['client', 'server']);
 
   api.use('http@1.2.9_1', ['client', 'server']);
 
