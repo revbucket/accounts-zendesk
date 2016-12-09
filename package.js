15c41cc15dd35dec3080866cb1968c2397f6998c
revbucket <mattjordan.mail@gmail.com>
2016-12-09 10:36:08 -0500

    Bumped dependency on revbucket:zendesk to 1.1.5

--- package.js
+++ package.js
@@ -1,8 +1,7 @@
 Package.describe({
   summary: "Login service for Zendesk accounts",
-  version: "1.1.2",
+  version: "1.1.3",
   name: "revbucket:accounts-zendesk",
-  version: "1.1.2",
   documentation: "README.md",
   git: "https://github.com/revbucket/accounts-zendesk.git"
 
@@ -14,7 +13,7 @@ Package.onUse(function(api) {
   // Export Accounts (etc) to packages using this one.
   api.imply('accounts-base@1.2.13', ['client', 'server']);
   api.use('accounts-oauth@1.1.13', ['client', 'server']);
-  api.use('revbucket:zendesk@1.1.4', ['client', 'server']);
+  api.use('revbucket:zendesk@1.1.5', ['client', 'server']);
 
   api.use('http@1.2.9_1', ['client', 'server']);
 
